3a20e2fa5468f3c9c3a379f08eeaa64fe6753bf7
Kevin Mulhern <kevinmulhern23@gmail.com>
2021-03-04 00:09:06 +0000

    Fix: Deleting a Lesson on Production Hangs
    Because:
    * Deleting a lesson loads all lesson_completions for that lesson into memory to delete them as well.
    
    This commit:
    * Adds a foreign key with a delete cascade so the database can handle deleting lesson completions associated with a lesson instead of Ruby.

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_134713) do
+ActiveRecord::Schema.define(version: 2021_03_03_234054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_134713) do
 
   add_foreign_key "flags", "project_submissions"
   add_foreign_key "flags", "users", column: "flagger_id"
+  add_foreign_key "lesson_completions", "lessons", on_delete: :cascade
   add_foreign_key "path_prerequisites", "paths"
   add_foreign_key "path_prerequisites", "paths", column: "prerequisite_id"
   add_foreign_key "project_submissions", "lessons"
